73ab999c454ba6fc432918426ed9ff7222cb0d3c
Cam205 <cameronbuchanan17@yahoo.com>
2021-11-01 13:03:21 -0500

    Finished Required Parts

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path');
+let app = express();
+
+app.use((req, res, next) => {
+    console.log(`The requested Original URL was ${req.originalUrl}`);
+    console.log(`The requested URL was ${req.url}`);
+    next()
+})
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res, next) => {
+    res.send('Hello from the web server side')
+    next()
+})
+
+
+app.listen(3000)
